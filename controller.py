d7b05bfd8742e31b523bb28feb6816bbc2568e23
Sam Wak <samuelwaknine@gmail.com>
2016-01-28 21:07:17 +0200

    Controller subscribe and auth fucntions

---
+++ controller.py
@@ -0,0 +1,26 @@
+from bottle import run, template, static_file, get, post, request
+
+db = DB()
+
+@post("/subscribe")
+def create_account():
+	firstName = request.forms.get('firstName')
+	lastName = request.forms.get('lastName')
+	email = request.forms.get('email')
+	password = request.forms.get('password')
+	program = request.forms.get('program')
+	skills = request.forms.get('skills')
+	job = request.forms.get('job')
+	country = request.forms.get('country')
+
+	db.subscribe(firstName, lastName, email, password, program, skills, job, country)
+
+
+@post("/auth")
+def authentificaion():
+	email = request.forms.get('email')
+	password = request.forms.get('password')
+
+	db.auth(email, password)
+
+run(host='localhost', port=7000, debug=True)
--- sam_test.py
+++
@@ -1,1 +0,0 @@
-sam_test.py
